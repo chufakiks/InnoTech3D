c600e31df93d74c3584fcb0f99cb0692d835f6ad
Bozhi <bozhilyu@gmail.com>
2024-10-17 09:41:24 +0200

    Pre-processing, RF models, Data Augmentation

--- demo.py
+++ demo.py
@@ -8,27 +8,27 @@ project2 = PointCloudProject(project='Project2')
 # project4 = PointCloudProject(project='Project4')
 
 
-# Draw point cloud and add Pipe bounding boxes
-o3d.visualization.draw_geometries([project2.pcd] + list(project2.objects['Pipe']))
+# # Draw point cloud and add Pipe bounding boxes
+# o3d.visualization.draw_geometries([project2.pcd] + list(project2.objects['Pipe']))
 
-# Draw point cloud and add bounding boxes for all three object types
-o3d.visualization.draw_geometries([project2.pcd]
-                                  + list(project2.objects['Pipe'])
-                                  + list(project2.objects['Structural_IBeam'])
-                                  + list(project2.objects['Structural_ColumnBeam'])
-                                  )
+# # Draw point cloud and add bounding boxes for all three object types
+# o3d.visualization.draw_geometries([project2.pcd]
+#                                   + list(project2.objects['Pipe'])
+#                                   + list(project2.objects['Structural_IBeam'])
+#                                   + list(project2.objects['Structural_ColumnBeam'])
+#                                   )
 
 
 # choose which object types to draw/crop from: 'Pipe', 'HVAC_Duct', 'Structural_ColumnBeam' or 'Structural_IBeam'
-which = ['Pipe', 'Structural_IBeam']
+which = ['Pipe']
 _ = project2.draw_bb_only(which, plot=True)
-_ = project2.draw_bb_only(which, masked=True, plot=True)
+# _ = project2.draw_bb_only(which, masked=True, plot=True)
 
 # Draw only subset of project2 as defined by Pipe and Structural_IBeam bounding boxes
 o3d.visualization.draw_geometries(project2.draw_bb_only(which, plot=False))
 
 # Draw entire point cloud with mask
-o3d.visualization.draw_geometries([project2.add_mask()])
+# o3d.visualization.draw_geometries([project2.add_mask()])
 
 # Draw entire point cloud, but removing all points belonging to bounding boxes of object types as defined below
 # This is very slow! Uncomment if execution is desirable!
